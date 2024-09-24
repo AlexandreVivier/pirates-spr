c66b46803a784596b702d772d9f83f48332ab744
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2024-09-24 17:34:10 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeBoard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeBoard from './components/HomeBoard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeBoard
   }
 }
 </script>
---
+++ src/components/HomeBoard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <h1>Pirates fight !</h1>
+        <p>
+        Bienvenue sur le jeu de combat de pirates !
+        </p>
+    </div>
+
+    <div class="container">
+        <h2>Vos points de vie :</h2>
+        <p>100</p>
+        <h2>Points de vie de l'ennemi :</h2>
+        <p>100</p>
+    </div>
+
+    <div class="container">
+        <h2>Choisissez votre action :</h2>
+        <button>Attaquer</button>
+        <button>Parer</button>
+        <button>Provoquer</button>
+    </div>
+
+    <div class="container">
+        <h2>Actions précédentes :</h2>
+        <ul>
+        </ul>
+    </div>
+
+</template>
+
+<script lang="js" setup>
+
+
+</script>
