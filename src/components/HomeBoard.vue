f0b8fd17806379db974e936e11827769f7c02b99
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2024-09-26 12:59:51 +0200

    compo historylogs

--- src/components/HomeBoard.vue
+++ src/components/HomeBoard.vue
@@ -9,17 +9,7 @@
 
         <ActionPannel @update-history="updateHistory"/>
 
-        <div class="flex flex-col justify-center items-center mb-4 bg-stone-200 w-1/2 py-2 px-4 border border-gray-400 rounded shadow">
-            <h2 class="font-semibold bastarda text-2xl py-2 border border-gray-300 px-4 rounded shadow text-center bg-stone-100 w-full">Actions précédentes :</h2>
-            <ul class="list-none">
-                <li class="list-none italic text-gray-800 text-center" v-for="(log, index) in logs" :key="index">
-                    <span v-if="log.includes('Egalité')" class="">{{ log }}</span>
-                    <span v-else-if="log.includes('gagné')" class="text-green-500">{{ log }}</span>
-                    <span v-else class="text-red-500">{{ log }}</span>
-                </li>
-            </ul>
-        </div>
-        
+        <HistoryLog :logs="logs"/>
     </div>
 </template>
 
@@ -27,7 +17,8 @@
 import { ref } from 'vue'
 import ActionPannel from './ActionPannel.vue'
 import HitPoints from './HitPoints.vue'
-import TitleCard from './TitleCard.vue';
+import TitleCard from './TitleCard.vue'
+import HistoryLog from './HistoryLog.vue'
 
 const playerHealth = ref(3)
 const enemyHealth = ref(3)
