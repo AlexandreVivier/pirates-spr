7be5974c42a53279e31577dcae6a4a9c37cdc071
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2024-12-17 17:44:51 +0100

    feature
    selectionScreen
    
    ISSUE XX

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <FightScreen />
+   <!-- <SelectionScreen /> -->
 </template>
 
 <script>
 import FightScreen from './components/FightScreen.vue'
+// import SelectionScreen from './components/SelectionScreen.vue';
 export default {
   name: 'App',
   components: {
-    FightScreen
+    FightScreen,
+    // SelectionScreen
   }
 }
 </script>
--- src/components/SelectionScreen.vue
+++ src/components/SelectionScreen.vue
@@ -1,6 +1,34 @@
 <template>
-<!-- Bienvenue. Ecran de séléction du pirate. Commencer le combat ! -->
+<div class="flex flex-col items-center w-screen bg-stone-100 w-full h-screen py-12 gap-8">
+    <TitleCard />
+    <div class="flex flex-col justify-center items-center w-full mb-4">
+    <p class="text-center italic text-2xl text-stone-500 pb-6">Sélectionnez votre pirate :</p>
+    <div class="flex justify-around items-center w-50 red-500 mb-2">
+        <img src="/images/portraits/portraitP.gif" alt="player picture" class="inline-block border-double border-8 filter grayscale hover:filter-none border-stone-500 hover:border-red-100 bg-stone-400 hover:bg-stone-300 hover:cursor-pointer transition ease-in-out delay-150"
+        @click="playerName = 'John'"/>
+        <img src="/images/portraits/portraitE.gif" alt="ennemy picture" class="inline-block border-double border-8 filter grayscale hover:filter-none border-stone-500 hover:border-red-100 bg-stone-400 hover:bg-stone-300 hover:cursor-pointer transition ease-in-out delay-150" 
+        @click="playerName = 'François'"/>
+    </div>
+    <p class="text-center italic text-2xl text-stone-500 pb-6">Je choisis "{{ playerName }}" !</p>
+    <CommonButton label="Commencer à jouer !"/>
+    </div>
+</div>
 </template>
 
 <script lang="js" setup>
+import { ref } from 'vue'
+import TitleCard from './TitleCard.vue'
+import CommonButton from './CommonButton.vue'
+const playerName = ref('') 
 </script>
+
+
+<style>
+.bastarda {
+    font-family: 'bastarda';
+}
+
+.text-stroke-3 {
+    -webkit-text-stroke:0.02em rgb(130, 66, 66);
+}
+</style>
