19430ec3e4be51e15131972f59f28b0eb95ec446
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2026-02-19 13:30:29 +0100

    cyp premier test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,7 +7,7 @@
 describe('début de partie',()=>{
   it('démarrer une partie simple', () => {
     cy.visit('http://localhost:8080/')
-    // cy.get(':nth-child(2) > .py-2').should('contain', 'Sélectionnez votre mode de jeu :')
+    cy.get(':nth-child(2) > .text-center').should('contain', 'Sélectionnez votre mode de jeu :')
     cy.get(':nth-child(2) > .mb-4').click()
     cy.get(':nth-child(2) > .py-2').should('contain', 'Partie simple')
     cy.get(':nth-child(2) > .italic').should('contain', 'Sélectionnez votre pirate :')
