cd1b5dec0a8e3a888884716931a9d5f07eeeaae2
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2026-02-19 13:36:21 +0100

    tofix adventure

--- src/components/AdventureTransition.vue
+++ src/components/AdventureTransition.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
         <div class="flex flex-col justify-center items-center w-full h-full bg-gradient-to-b from-stone-900 via-slate-600 to-stone-900 text-stone-200">
             <div class="flex flex-col justify-center items-center w-3/4 bg-yellow-500 border-4 border-stone-800 rounded-lg p-4 mb-4">
                 <p class="text-3xl font-bold text-center bastarda text-stone-900">Boutique de l'aventurier</p>
@@ -26,7 +26,7 @@ function updateAdventure() {
       ennemyName.value = adventure.value.ennemies[0] || null
 }
 updateAdventure();
-</script> -->
+</script>
 
 
 <!-- 
